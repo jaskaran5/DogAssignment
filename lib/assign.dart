3149943e90dd9a8373c23d1296635478b45615cc
jaskaran <108335710+jaskaran5@users.noreply.github.com>
2026-05-15 17:59:56 +0530

    initial commit

---
+++ lib/assign.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Assignment extends StatefulWidget {
+  @override
+  AssignmenentState createState() => AssignmenentState();
+}
+
+class AssignmenentState extends State<Assignment> {
+  /// call one time after object creation .
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant Assignment oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Data ')));
+  }
+}
